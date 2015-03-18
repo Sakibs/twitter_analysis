ae7a9f7dc2337df777b8f92a067919bff713a591
Sakibs <sakibs8@gmail.com>
2015-03-17 23:19:01 -0700

    Fix status initialization

--- tweet_analysis.py
+++ tweet_analysis.py
@@ -19,6 +19,14 @@ def get_TOD(tstamp):
 	clk = elems[1].split(':')
 	return int(clk[0])
 
+def getHourStart(tstamp):
+	tstr = TStoDT(tstamp)
+	elems = tstr.split(' ')
+	clk = elems[1].split(':')
+	seconds = int(clk[1])*60+int(clk[2])
+
+	return tstamp-seconds
+
 def store_stats_data(hashtag, hours, stats):
 	out = open(os.path.join('.', 'part2', 'stats_'+hashtag+'.txt'), 'w')
 	# for hr in hours:
@@ -38,6 +46,16 @@ def load_stats_data(hashtag):
 
 	return stats_list
 
+def update_stat_item(tweet, stat):
+	tweet_time = tweet['firstpost_date']
+	retweet_count = tweet['metrics']['citations']['total']
+	user_followers = tweet['original_author']['followers']
+
+	stat['n_tweets'] += 1
+	stat['n_retweets'] += retweet_count
+	stat['num_follwr'] += user_followers
+	stat['maxn_follwr'] = max(stat['maxn_follwr'], user_followers)
+
 def get_tweet_stats(hashtag):
 	filename = 'tweets_'+hashtag+'.txt'
 	filepath = os.path.join('.', 'data', filename)
@@ -51,37 +69,26 @@ def get_tweet_stats(hashtag):
 	tweet = json.loads(f.readline())
 
 	retweet_count = tweet['metrics']['citations']['total']
-	user_followers = tweet['original_author']['followers']
-	hour_start = tweet['firstpost_date']
+	hour_start = getHourStart( tweet['firstpost_date'] )
 	hour_end = hour_start + 3600
 
 	current_stats = {
 		'hour_start'		: hour_start,			# hour start and end window
 		'hour_end'			: hour_end,				
-		'n_tweets' 			: 1,					# count of # of tweets in hour span
-		'n_retweets' 		: retweet_count,		# total # of retweets ** Verify
-		'num_follwr'		: user_followers,		# total # of followers of users posting this hashtag
-		'maxn_follwr'		: user_followers,		# max # followers in users ** NOT SURE YET
+		'n_tweets' 			: 0,					# count of # of tweets in hour span
+		'n_retweets' 		: 0,		# total # of retweets ** Verify
+		'num_follwr'		: 0,		# total # of followers of users posting this hashtag
+		'maxn_follwr'		: 0,		# max # followers in users ** NOT SURE YET
 		'sum_follwr_post'	: 0, 					# sum of followers posting hashtag ** NOT SURE YET
 		'tod'				: get_TOD(hour_start)
 	}
 
 	while 1:
-		line = f.readline()
-		if line == '':
-			break
-
-		tweet = json.loads(line)
 		tweet_time = tweet['firstpost_date']
-		retweet_count = tweet['metrics']['citations']['total']
-		user_followers = tweet['original_author']['followers']
-
+	
 		if tweet_time >= hour_start and tweet_time < hour_end:
 			# update current_stats
-			current_stats['n_tweets'] += 1
-			current_stats['n_retweets'] += retweet_count
-			current_stats['num_follwr'] += user_followers
-			current_stats['maxn_follwr'] = max(current_stats['maxn_follwr'], user_followers)
+			update_stat_item(tweet, current_stats)
 		else:
 			# setup for next window
 			# append hour to hours list. This is to keep track of order
@@ -91,19 +98,37 @@ def get_tweet_stats(hashtag):
 			#print '[' + str(hour_start) + ' to ' + str(hour_end) + ')\t' + str(current_stats[hour_start])
 			out.write(str(hour_start) + '\t' + str(hour_end) + '\t' + str(current_stats['n_tweets']) + '\n' )
 			
-			hour_start = hour_end
-			hour_end = hour_start + 3600
-			# reset current_stats
-			current_stats = {
-				'hour_start'		: hour_start,			# hour start and end window
-				'hour_end'			: hour_end,				
-				'n_tweets' 			: 1,					# count of number of tweets in hour span
-				'n_retweets' 		: retweet_count,		# total number of retweets ** Verify
-				'num_follwr'		: user_followers,		# total # of followers of users posting this hashtag
-				'maxn_follwr'		: user_followers,		# max # followers in users ** NOT SURE YET
-				'sum_follwr_post'	: 0, 					# sum of followers posting hashtag ** NOT SURE YET
-				'tod'				: get_TOD(hour_start)
-			}
+			found_interval = False
+			while not found_interval:
+				hour_start = hour_end
+				hour_end = hour_start + 3600
+
+				# reset current_stats
+				current_stats = {
+					'hour_start'		: hour_start,			# hour start and end window
+					'hour_end'			: hour_end,				
+					'n_tweets' 			: 0,					# count of number of tweets in hour span
+					'n_retweets' 		: 0,		# total number of retweets ** Verify
+					'num_follwr'		: 0,		# total # of followers of users posting this hashtag
+					'maxn_follwr'		: 0,		# max # followers in users ** NOT SURE YET
+					'sum_follwr_post'	: 0, 					# sum of followers posting hashtag ** NOT SURE YET
+					'tod'				: get_TOD(hour_start)
+				}
+
+				if tweet_time >= hour_start and tweet_time < hour_end:
+					found_interval = True
+				else:
+					hour_list.append(hour_start)
+					stats_list.append(current_stats)
+					out.write(str(hour_start) + '\t' + str(hour_end) + '\t' + str(current_stats['n_tweets']) + '\n' )
+
+			update_stat_item(tweet, current_stats)
+
+		line = f.readline()
+		if line == '':
+			break
+
+		tweet = json.loads(line)
 
 	# append the last stats to the list
 	hour_list.append(hour_start)
@@ -176,12 +201,13 @@ def cross_validate(tweet_stats):
 if __name__ == "__main__":
 	hashtags = ['#superbowl', '#nfl', '#gopatriots'];
 
-	#tweet_stats = get_tweet_stats(hashtags[2])
-	tweet_stats = load_stats_data(hashtags[1])
-	cross_validate(tweet_stats)
+	tweet_stats = get_tweet_stats(hashtags[2])
+	#tweet_stats = load_stats_data(hashtags[1])
+	#cross_validate(tweet_stats)
 
 	#time_idx =np.arange(0,611)
 	#model = get_regression_model(tweet_stats,time_idx)
+
 
 	#results = model.fit()
 	#print (results.summary())
